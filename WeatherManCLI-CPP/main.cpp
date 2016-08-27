00de489f9fd6193035151c95198b7e600fe5a7fa
Eric Dejonckheere <eric@aya.io>
2016-08-27 12:43:48 +0200

    Initial Commit

---
+++ WeatherManCLI-CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WeatherManCLI-CPP
+//
+//  Created by ERIC DEJONCKHEERE on 27/08/2016.
+//  Copyright © 2016 AYA.io. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
